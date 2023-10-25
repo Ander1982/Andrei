5545c489375d675c1502be432d47af686f7d8f8b
Andrei1982 <pasevich.andrei@yandex.ru>
2023-10-25 20:43:43 +0200

    DZ_35

---
+++ Урок 35.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# con = sqlite3.connect("profile.db")
+# cur = con.cursor()
+#
+# cur.execute("""""")
+# con.close()
+
+with sqlite3.connect("profile.db") as con:
+    cur = con.cursor()
+    # cur.execute("""CREATE TABLE IF NOT EXIST users(
+    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     name TEXT NOT NULL,
+    #     summa REAL,
+    #     date BLOB
+    # )""")
+    # cur.execute("DROP TABLE users")
+
